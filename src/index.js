484e9a98d1bae2c33e8270602cf4c175a7e861e5
liaoyanhua <1457883120@qq.com>
2024-03-26 20:54:18 +0800

    完成@路径配置

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import sum from '@/test'
+console.log(sum())
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
---
+++ src/test.js
@@ -0,0 +1,5 @@
+export default function sum() {
+  const a = 3
+  const b = 4
+  return a + b
+}
