dd6bff770cde9fa081a17349926ea76a2b3fdfb4
liaoyanhua <1457883120@qq.com>
2024-03-27 10:33:30 +0800

    antd主题颜色定制完成

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from './router'
 import { RouterProvider } from 'react-router-dom'
+import './theme.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<RouterProvider router={router} />)
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import { Button } from 'antd-mobile'
 
 const Layout = () => {
   return (
     <div>
       <Outlet />
       我是Layout
+      <Button color="primary">全局测试</Button>
+      <div className="purple">
+        <Button color="primary">局部测试</Button>
+      </div>
     </div>
   )
 }
