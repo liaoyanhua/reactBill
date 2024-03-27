1a16418f4ab7366e77826d1d51d7d1746d00e4a3
liaoyanhua <1457883120@qq.com>
2024-03-27 17:31:59 +0800

    TabBar功能实现完成

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,22 +1,63 @@
-import { Outlet } from 'react-router-dom'
-import { Button } from 'antd-mobile'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { TabBar } from 'antd-mobile'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { getBillList } from '@/store/modules/billStore'
+import './index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from 'antd-mobile-icons'
+
+const tabs = [
+  {
+    key: '/',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 const Layout = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getBillList())
   })
+  const navigate = useNavigate()
+  const toPageRouter = (path) => {
+    console.log(path)
+    navigate(path)
+  }
   return (
-    <div>
-      <Outlet />
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={toPageRouter}>
+          {tabs.map((item) => {
+            return (
+              <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+            )
+          })}
+        </TabBar>
+      </div>
+      {/* <Outlet />
       我是Layout
       <Button color="primary">全局测试</Button>
       <div className="purple">
         <Button color="primary">局部测试</Button>
-      </div>
+      </div> */}
     </div>
   )
 }
